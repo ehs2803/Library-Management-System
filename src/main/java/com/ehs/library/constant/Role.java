f22c502fd6e9747874baff49c0553ae0683c8d72
ehs2803 <hr2803@naver.com>
2022-06-20 17:18:32 +0900

    mysql connect-20220620

---
+++ src/main/java/com/ehs/library/constant/Role.java
@@ -0,0 +1,5 @@
+package com.ehs.library.constant;
+
+public enum Role {
+    USER, ADMIN
+}
---
+++ src/main/java/com/ehs/library/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.ehs.library.repository;
+
+import com.ehs.library.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    Member findByEmail(String email);
+
+}

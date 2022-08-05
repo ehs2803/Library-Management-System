85a66d8aca2bef9f9f07234f81b586bd7eda7f21
ehs2803 <hr2803@naver.com>
2022-08-05 22:50:53 +0900

    통합검색-20220805

--- src/main/java/com/ehs/library/book/controller/BookController.java
+++ src/main/java/com/ehs/library/book/controller/BookController.java
@@ -1,45 +1,50 @@
 package com.ehs.library.book.controller;
 
+import com.ehs.library.book.service.BookService;
 import com.ehs.library.bookhope.dto.BookHopeFormDto;
 import com.ehs.library.bookhope.service.BookHopeService;
+import com.ehs.library.member.entity.Member;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.validation.Valid;
 import java.security.Principal;
 
-//@Controller
-//@RequestMapping(value = "/book")
-//@RequiredArgsConstructor
+@Controller
+@RequestMapping(value = "/main/book")
+@RequiredArgsConstructor
 public class BookController {
-//    private final BookHopeService bookHopeService;
+    private final BookService bookService;
+
+    @GetMapping(value = "/search")
+    public String searchBookList(){
+
+
+        return "book/searchBook";
+    }
+
+    @GetMapping("/search/result")
+    public String bookList(@RequestParam(defaultValue = "") String keyword,
+                             @PageableDefault(sort = "email", direction = Sort.Direction.ASC) Pageable pageable,
+                             Model model){
+//        Page<Member> memberList = memberService.memberUserLoanList(keyword, pageable);
 //
-//    @GetMapping("hope/register")
-//    public String registerHopeBookForm(Model model){
-//        model.addAttribute("bookHopeFormDto", new BookHopeFormDto());
-//        return "book/addHopeBookForm";
-//    }
-//
-//    @PostMapping("hope/register")
-//    public String registerHopeBook(@Valid BookHopeFormDto bookHopeFormDto, BindingResult bindingResult,
-//                                   Model model, Principal principal){
-//        if(bindingResult.hasErrors()){
-//            return "book/addHopeBookForm";
-//        }
-//
-//        try {
-//            String email = principal.getName();
-//            bookHopeService.registerBookHope(bookHopeFormDto, email);
-//        } catch (Exception e){
-//            model.addAttribute("errorMessage", "상품 등록 중 에러가 발생하였습니다.");
-//            return "book/addHopeBookForm";
-//        }
-//
-//        return "redirect:/member/mypage";
-//    }
+//        model.addAttribute("memberList", memberList);
+//        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+//        model.addAttribute("next", pageable.next().getPageNumber());
+//        model.addAttribute("hasNext", memberList.hasNext());
+//        model.addAttribute("hasPrev", memberList.hasPrevious());
+
+        return "book/searchBookList";
+    }
 }

7064e1efe7aca969b923e50bea76f717e3215f7d
ehs2803 <hr2803@naver.com>
2022-08-20 18:30:14 +0900

    deploy.sh 수정-20220820

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,9 @@
 REPOSITORY=/home/ec2-user/app/deploy
 PROJECT_NAME=freelec-springboot2-webservice
 
-echo "> Build 파일 복사"
-
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+#echo "> Build 파일 복사"
+#
+#cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동중인 애플리케이션 pid 확인"
 

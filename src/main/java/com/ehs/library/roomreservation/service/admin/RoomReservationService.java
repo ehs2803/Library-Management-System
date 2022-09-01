5e7a686955fb444a1ebd5bf02d4f4bba8255c902
ehs2803 <hr2803@naver.com>
2022-09-01 14:50:53 +0900

    feat : 스터디룸 수정, 중단/재개 기능 구현

--- src/main/java/com/ehs/library/roomreservation/service/admin/RoomReservationService.java
+++ src/main/java/com/ehs/library/roomreservation/service/admin/RoomReservationService.java
@@ -1,16 +1,17 @@
 package com.ehs.library.roomreservation.service.admin;
 
-import com.ehs.library.member.entity.Member;
 import com.ehs.library.roomreservation.constant.StudyRoomState;
-import com.ehs.library.roomreservation.dto.StudyRoomAddFormDto;
+import com.ehs.library.roomreservation.dto.StudyRoomFormDto;
 import com.ehs.library.roomreservation.entity.StudyRoom;
 import com.ehs.library.roomreservation.repository.StudyRoomRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service("admin.RoomReservationService")
+@Transactional
 @RequiredArgsConstructor
 public class RoomReservationService {
     private final StudyRoomRepository studyRoomRepository;
@@ -19,7 +20,7 @@ public class RoomReservationService {
         return studyRoomRepository.findAll();
     }
 
-    public StudyRoom registerStudyRoom(StudyRoomAddFormDto studyRoom){
+    public StudyRoom registerStudyRoom(StudyRoomFormDto studyRoom){
         StudyRoom validStudyRoom = studyRoomRepository.findByName(studyRoom.getName());
         if(validStudyRoom != null){
             throw new IllegalStateException("존재하는 스터디룸 이름입니다.");
@@ -33,5 +34,35 @@ public class RoomReservationService {
                 .build();
 
         return studyRoomRepository.save(registerStudyRoom);
+    }
+
+    public Long changeStateStudyRoom(Long id){
+        StudyRoom studyRoom = studyRoomRepository.findById(id).get();
+        if(studyRoom.getState().toString().equals("AVAILABLE")){
+            studyRoom.setState(StudyRoomState.CLOSE);
+        }
+        else{
+            studyRoom.setState(StudyRoomState.AVAILABLE);
+        }
+        return studyRoom.getId();
+    }
+
+    public StudyRoomFormDto getStudyRoom(Long id){
+        StudyRoom studyRoom = studyRoomRepository.findById(id).get();
+        StudyRoomFormDto studyRoomFormDto = StudyRoomFormDto.builder()
+                .id(studyRoom.getId())
+                .name(studyRoom.getName())
+                .location(studyRoom.getLocation())
+                .capacity(studyRoom.getCapacity())
+                .build();
+        return studyRoomFormDto;
+    }
+
+    public StudyRoom editStudyRoom(StudyRoomFormDto studyRoomFormDto){
+        StudyRoom studyRoom = studyRoomRepository.findById(studyRoomFormDto.getId()).get();
+        studyRoom.setName(studyRoomFormDto.getName());
+        studyRoom.setLocation(studyRoomFormDto.getLocation());
+        studyRoom.setCapacity(studyRoomFormDto.getCapacity());
+        return studyRoom;
     }
 }

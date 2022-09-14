64ac82df7bb8d2dd68f4840765f7b8acfdb40ed0
ehs2803 <hr2803@naver.com>
2022-09-14 09:26:31 +0900

    refactor : bookhope-user package

--- src/main/java/com/ehs/library/bookhope/controller/user/BookHopeController.java
+++ src/main/java/com/ehs/library/bookhope/controller/user/BookHopeController.java
@@ -2,38 +2,41 @@ package com.ehs.library.bookhope.controller.user;
 
 import com.ehs.library.book.entity.Book;
 import com.ehs.library.bookhope.constant.BookHopeState;
+import com.ehs.library.bookhope.dto.BookHopeDetailDto;
+import com.ehs.library.bookhope.dto.BookHopeDto;
 import com.ehs.library.bookhope.dto.BookHopeFormDto;
 import com.ehs.library.bookhope.dto.BookHopeMapperDto;
 import com.ehs.library.bookhope.entity.BookHope;
 import com.ehs.library.bookhope.repository.BookHopeMapperRepository;
 import com.ehs.library.bookhope.service.user.BookHopeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
 import java.security.Principal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller("user.BookHopeController")
 @RequiredArgsConstructor
 public class BookHopeController {
     private final BookHopeService bookHopeService;
-
     private final BookHopeMapperRepository bookHopeMapperRepository;
 
+    // 희망도서 신청 폼
     @GetMapping("/book/hope/register")
     public String registerHopeBookForm(Model model){
         model.addAttribute("bookHopeFormDto", new BookHopeFormDto());
         return "book/addHopeBookForm";
     }
 
+    // 희망도서 신청
     @PostMapping("/book/hope/register")
     public String registerHopeBook(@Valid BookHopeFormDto bookHopeFormDto, BindingResult bindingResult,
                                    Model model, Principal principal){
@@ -52,14 +55,23 @@ public class BookHopeController {
         return "redirect:/member/mypage";
     }
 
+    // 멤버 마이페이지에서 희망도서 신청 목록 조회
     @GetMapping("/member/mypage/book/hope/list")
     public String memberBookHopeList(Model model, Principal principal){
         String email = principal.getName();
         Long memberId = bookHopeService.getMemberId(email);
 
-        List<BookHope> bookHopeListComplete = bookHopeService.findByMemberAndState(email, BookHopeState.COMPLETE);
-        List<BookHope> bookHopeListReject = bookHopeService.findByMemberAndState(email, BookHopeState.REJECT);
+        List<BookHope> bookHopeListComplete_entity = bookHopeService.findByMemberAndState(email, BookHopeState.COMPLETE);
+        List<BookHope> bookHopeListReject_entity = bookHopeService.findByMemberAndState(email, BookHopeState.REJECT);
         List<BookHopeMapperDto> bookHopeMapperDtoList = bookHopeMapperRepository.findProgressBookHope(memberId);
+
+        ModelMapper modelMapper = new ModelMapper(); // ModelMapper이용해 List<Entity> -> List<Dto>
+        List<BookHopeDto> bookHopeListComplete = bookHopeListComplete_entity.stream()
+                .map(bookHope->modelMapper.map(bookHope, BookHopeDto.class))
+                .collect(Collectors.toList());
+        List<BookHopeDto> bookHopeListReject = bookHopeListReject_entity.stream()
+                .map(bookHope->modelMapper.map(bookHope, BookHopeDto.class))
+                .collect(Collectors.toList());
 
         model.addAttribute("bookHopeListComplete", bookHopeListComplete);
         model.addAttribute("bookHopeListReject", bookHopeListReject);
@@ -68,25 +80,67 @@ public class BookHopeController {
         return "bookhope/user/userBookHopeList";
     }
 
+    // 거절된 희망도서 신청 상세보기
     @GetMapping("/member/mypage/book/hope/reject/{id}")
     public String memberBookHopeReject(@PathVariable Long id, Model model){
-        BookHope bookHope = bookHopeService.findById(id);
+        BookHope bookHope_entity = bookHopeService.findById(id);
+        BookHopeDetailDto bookHope = BookHopeDetailDto.builder()
+                .id(bookHope_entity.getId())
+                .name(bookHope_entity.getName())
+                .isbn(bookHope_entity.getIsbn())
+                .author(bookHope_entity.getAuthor())
+                .publisher(bookHope_entity.getPublisher())
+                .year(bookHope_entity.getYear())
+                .state(bookHope_entity.getState().toString())
+                .failReason(bookHope_entity.getFailReason())
+                .rejectTime(bookHope_entity.getRejectTime())
+                .build();
+
         model.addAttribute("bookHope", bookHope);
 
         return "bookhope/user/BookHopeRejectDetail";
     }
 
+    // 완료처리된 희망도서 신청 상세보기
     @GetMapping("/member/mypage/book/hope/complete/{id}")
     public String memberBookHopeComplete(@PathVariable Long id, Model model){
-        BookHope bookHope = bookHopeService.findById(id);
+        BookHope bookHope_entity = bookHopeService.findById(id);
+        BookHopeDetailDto bookHope = BookHopeDetailDto.builder()
+                .id(bookHope_entity.getId())
+                .name(bookHope_entity.getName())
+                .isbn(bookHope_entity.getIsbn())
+                .author(bookHope_entity.getAuthor())
+                .publisher(bookHope_entity.getPublisher())
+                .year(bookHope_entity.getYear())
+                .state(bookHope_entity.getState().toString())
+                .allowTime(bookHope_entity.getAllowTime())
+                .shippingTime(bookHope_entity.getShippingTime())
+                .arrangeTime(bookHope_entity.getArrangeTime())
+                .completeTime(bookHope_entity.getCompleteTime())
+                .build();
+
         model.addAttribute("bookHope", bookHope);
 
         return "bookhope/user/BookHopeCompleteDetail";
     }
 
+    // 진행중인 희망도서 신청 상세보기
     @GetMapping("/member/mypage/book/hope/progress/{id}")
     public String memberBookHopeProgress(@PathVariable Long id, Model model){
-        BookHope bookHope = bookHopeService.findById(id);
+        BookHope bookHope_entity = bookHopeService.findById(id);
+        BookHopeDetailDto bookHope = BookHopeDetailDto.builder()
+                .id(bookHope_entity.getId())
+                .name(bookHope_entity.getName())
+                .isbn(bookHope_entity.getIsbn())
+                .author(bookHope_entity.getAuthor())
+                .publisher(bookHope_entity.getPublisher())
+                .year(bookHope_entity.getYear())
+                .state(bookHope_entity.getState().toString())
+                .allowTime(bookHope_entity.getAllowTime())
+                .shippingTime(bookHope_entity.getShippingTime())
+                .arrangeTime(bookHope_entity.getArrangeTime())
+                .build();
+
         model.addAttribute("bookHope", bookHope);
 
         return "bookhope/user/BookHopeProgressDetail";
--- src/main/java/com/ehs/library/bookhope/dto/BookHopeDto.java
+++ src/main/java/com/ehs/library/bookhope/dto/BookHopeDto.java
@@ -11,6 +11,7 @@ import java.time.LocalDateTime;
 public class BookHopeDto {
     private Long id;
     private String name;
+    private String isbn;
     private LocalDateTime regTime;
     private MemberSimpleDto user;
 }
